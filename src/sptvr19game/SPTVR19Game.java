655f23afd2b4b813e47a0c3835692e68e9e8e1b8
roman ivanov <roman.ivanov@ivkhk.ee>
2020-09-16 09:03:53 +0300

    заготовка проекта

---
+++ src/sptvr19game/SPTVR19Game.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19game;
+
+/**
+ *
+ * @author user
+ */
+public class SPTVR19Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+    
+}
