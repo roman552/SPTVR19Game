cf243be5e876907f57384a58d6e67c47b5fef029
roman ivanov <roman.ivanov@ivkhk.ee>
2020-09-16 09:17:26 +0300

    первая версия программы без рандома

--- src/sptvr19game/App.java
+++ src/sptvr19game/App.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package sptvr19game;
-
+import java.util.Scanner;
 /**
  *
  * @author user
@@ -12,5 +12,15 @@ package sptvr19game;
 class App {
     public void run() {
         System.out.println("Угадай задуманное число");
+        System.out.println("-------------------------");
+        int myNumber = 2;
+        System.out.println("Задумано число от 0 до 5. Угадай!");
+        Scanner scanner = new Scanner(System.in);
+        int gamerNumber = scanner.nextInt();
+        if (myNumber == gamerNumber) {
+            System.out.println("Ты выйграл!");
+        } else {
+            System.out.println("Ты проиграл. Задумано число: "+myNumber);
+        }
     }
 }
